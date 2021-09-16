c2e4dc933264015ac3893dfb47246938a5b036b3
Fábio <fabio.mendes@profusion.com>
2021-09-15 22:01:21 -0300

    add: more responsive components, navigation and active link

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,23 +1,27 @@
 import {
-  Link,
+  Link as ChakraLink,
   Icon,
   Text,
   LinkProps as ChakraLinkProps,
 } from "@chakra-ui/react";
 import { IconType } from "react-icons";
+import { ActiveLink } from "../ActiveLink";
 
 interface NavLinkProps extends ChakraLinkProps {
   children: string;
   icon: IconType;
+  href: string;
 }
 
-export function NavLink({ children, icon, ...rest }: NavLinkProps) {
+export function NavLink({ children, icon, href, ...rest }: NavLinkProps) {
   return (
-    <Link display="flex" align="center" {...rest}>
-      <Icon as={icon} />
-      <Text ml="4" fontWeight="medium">
-        {children}
-      </Text>
-    </Link>
+    <ActiveLink href={href} passHref>
+      <ChakraLink display="flex" align="center" {...rest}>
+        <Icon as={icon} />
+        <Text ml="4" fontWeight="medium">
+          {children}
+        </Text>
+      </ChakraLink>
+    </ActiveLink>
   );
 }
