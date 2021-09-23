5a44539cfffcf15d5d7bf043d07c670e3a460b8a
Fábio <fabio.mendes@profusion.com>
2021-09-22 21:22:53 -0300

    feat: add miragejs and faker

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -14,7 +14,7 @@ import {
   Tr,
   useBreakpointValue,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
@@ -26,6 +26,13 @@ export default function UserList() {
     base: false,
     lg: true,
   });
+
+  useEffect(() => {
+    fetch("hhtp://localhost:3000/api/users")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <Box>
       <Header />
